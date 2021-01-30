009b846d1a23a913e5468204bc03197c2cc74ac5
cod3rcarl <cod3rcarl@gmail.com>
2021-01-30 08:29:24 +0000

    environment variables

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import Search from "../Nav/search";
 const HomePage = ({ user, search, newsData, setNews, handleNewSearch }) => {
   const { userName } = user;
   const firstName = userName.split(" ").slice(0, 1);
-  const apiKey = "ef7fd608-e4ba-4e7d-b63c-8fcc4fd8f9f1";
+  const apiKey = process.env.REACT_APP_GUARDIAN_API_KEY;
 
   useEffect(() => {
     async function getNews() {
@@ -42,7 +42,10 @@ const HomePage = ({ user, search, newsData, setNews, handleNewSearch }) => {
         {search ? (
           <h2>
             Top Stories related to{" "}
-            {`${search.slice(2).charAt(0).toUpperCase()}${search.slice(3)}`}
+            {`${search
+              .slice(2)
+              .charAt(0)
+              .toUpperCase()}${search.slice(3)}`}
           </h2>
         ) : (
           <h2>Top Stories</h2>
